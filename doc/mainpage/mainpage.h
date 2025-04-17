f85ebe23b288a5df73ab04d42afa4964b21d1e60
lishifeng <lishifenging@outlook.com>
2025-04-18 01:09:52 +0800

    fix: fix doc errors

--- doc/mainpage/mainpage.h
+++ doc/mainpage/mainpage.h
@@ -1,10 +1,10 @@
 /**
  * @mainpage  LibDriver MPU9250
- * @brief     LibDriver MPU9250 is a full function driver of MPU9250 and it can run in both MCU and Linux platform.
+ * @brief     LibDriver MPU9250 is a full-featured driver of MPU9250 and it can run in both MCU and Linux platform.
  * @version   1.0.0
  * @author    Shifeng Li
  *
- * <h3>history</h3>
+ * <h3>History</h3>
  * <table>
  * <tr><th>Date        <th>Version  <th>Author      <th>Description
  * <tr><td>2022/08/30  <td>1.0      <td>Shifeng Li  <td>first upload
